937cda5ed899f6d5eb79de88f2ac8d20e268bc2c
han18 <han.fadah@gmail.com>
2023-12-10 20:34:37 -0500

    added a background image

--- SRC/script.js
+++ SRC/script.js
@@ -28,13 +28,6 @@ for (let x = 0; x < menuLinks.length; x++) {
   navMenu.appendChild(aLink);
 }
 // =============== here is the form validation part
-// validating the form
-// const formVal = document.getElementById("form-validation");
-
-// formVal.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   alert("The form has been submitted successfully");
-// });
 
 // validating the form and clearing the input fields
 let output = document.querySelector(".message-output");
@@ -44,33 +37,3 @@ formReset.addEventListener("submit", (e) => {
   formReset.reset();
   output.innerHTML += "The Form is submitted successfully!";
 });
-
-//  const clickButton = document.getElementById("button-submit");
-//   //////// I stopped here... the form didn't display a message
-// clickButton.addEventListener("click", () => {
-//   clickButton.textContent = "HHHHHHHHHHHH";
-//
-//   output.textContent = "GGGGGGGGGGGGGG";
-// showing a message to the user
-// const buttonInput = document.getElementById("button-submit");
-// let output = document.querySelector(".message-output");
-
-// output.addEventListener("click", () => {
-//   output.style.backgroundColor = "pink";
-//   output.style.color = "red";
-//   buttonInput.textContent = "WE ARE SO REAL";
-// });
-
-// const clickButton = document.getElementById("button-submit");
-// clickButton.addEventListener("submit", () => {
-//   // clickButton.textContent = "HHHHHHHHHHHH";
-//   // let output = document.querySelector(".message-output");
-//   // output.textContent = "GGGGGGGGGGGGGG";
-// });
-
-// outputs.innerHTML += "Yes Your Form has been submitted";
-// function clearInputs(event) {
-//   const allInputs = document.querySelectorAll("input");
-//   allInputs.forEach((input) => (input.value = ""));
-//   outputs.textContent = "Yes Your Form has been submitted";
-// }
